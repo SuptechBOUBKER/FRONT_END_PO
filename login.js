1ad3782d5815426b5604a8b5fbb270da9345ad25
BOUBKERsup07 <nikaimkjf289@gmail.com>
2024-06-29 20:59:14 +0100

    o

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 async function register(event) {
     event.preventDefault();
-    
+
     const name = document.getElementById('register-name').value;
     const number = document.getElementById('register-number').value;
     const order = document.getElementById('register-order').value;
@@ -9,7 +9,7 @@ async function register(event) {
     const dateTime = document.getElementById('register-dateTime').value;
     const address = document.getElementById('register-address').value;
     const message = document.getElementById('register-message').value;
-  
+
     const consumer = {
         name: name,
         number: number,
@@ -20,27 +20,28 @@ async function register(event) {
         address: address,
         message: message
     };
-  
+
     try {
-      const response = await fetch('http://localhost:8080/POproject/resto', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(consumer)
-      });
-  
-      if (response.ok) {
-        alert('Enregistrement réussi!');
-      } else {
-        alert('Échec de l\'enregistrement!');
-      }
+        const response = await fetch('http://localhost:8080/POproject/resto', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(consumer)
+        });
+
+        if (response.ok) {
+            alert('Enregistrement réussi!');
+            // Vous pouvez ajouter ici des actions supplémentaires après l'enregistrement réussi, si nécessaire.
+        } else {
+            alert('Échec de l\'enregistrement!');
+        }
     } catch (error) {
-      console.error('Erreur:', error);
-      alert('Erreur de serveur!');
+        console.error('Erreur:', error);
+        alert('Erreur de serveur!');
     }
 }
-  
+
 async function login(event) {
     event.preventDefault();
     
@@ -54,9 +55,10 @@ async function login(event) {
       const user = consumers.find(consumer => consumer.name === name && consumer.number === number);
       
       if (user) {
-        alert('Commande réussie!');
+        alert('Connexion réussie!'); // Message adapté pour la connexion réussie
+        // Ajoutez ici des actions supplémentaires après la connexion réussie, si nécessaire.
       } else {
-        alert('invalide');
+        alert('Nom d\'utilisateur ou numéro invalide');
       }
     } catch (error) {
       console.error('Erreur:', error);
