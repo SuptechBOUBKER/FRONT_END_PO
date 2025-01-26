c9d8b0a1b99404339994c90a3bf2c8492e835ab3
BOUBKERsup07 <nikaimkjf289@gmail.com>
2025-01-26 23:35:35 +0100

    version1

--- script.js
+++ script.js
@@ -44,34 +44,3 @@ window.onscroll = () => {
         }
     });
 };
-
-
-// Configuration de Swiper pour la section des avis
-var swiper = new Swiper(".review-slider", {
-    spaceBetween: 10, // Espace entre chaque diapositive en pixels
-    centeredSlides: false, // Les diapositives ne seront pas centrées
-    autoplay: {
-        delay: 6500, // Délai entre les défilements automatiques en millisecondes
-        disableOnInteraction: false, // Continuer l'autoplay même après interaction
-    },
-    loop: true, // Permet de faire défiler les diapositives en boucle
-    breakpoints: {
-        0: {
-            slidesPerView: 1, // 1 diapositive vue sur les petits écrans
-            spaceBetween: 10 // Moins d'espace pour les petits écrans
-        },
-        640: {
-            slidesPerView: 1, // 2 diapositives vues sur les écrans moyens
-            spaceBetween: 10 // Un peu plus d'espace pour les écrans moyens
-        },
-        1024: {
-            slidesPerView: 2, // 3 diapositives vues sur les grands écrans
-            spaceBetween: 10 // Plus d'espace pour les grands écrans
-        },
-        1440: {
-            slidesPerView: 4, // 4 diapositives vues sur les très grands écrans
-            spaceBetween: 10 // Encore plus d'espace pour les très grands écrans
-        }
-    }
-});
-
